25c54fb540351bced798986cbcffa43f22f83638
student <student@iMac-student.local>
2020-06-03 23:01:41 +0300

    header

--- includes/lemin.h
+++ includes/lemin.h
@@ -13,10 +13,10 @@
 #ifndef LEMIN_H
 # define LEMIN_H
 
-# include "../libft/libft.h"
+# include "libft.h"
 # include <stdlib.h>
 # include <stdio.h>
-# include "../libft/get_next_line.h"
+# include "get_next_line.h"
 
 typedef struct			s_room
 {
